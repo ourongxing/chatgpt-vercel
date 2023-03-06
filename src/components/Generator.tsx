be2b839c9941ea65e7d6bf83ea3e092effa7b36a
yicaoli <105924154+yicaoli@users.noreply.github.com>
2023-03-06 18:43:25 +0800

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -9,7 +9,7 @@ import { Fzf } from "fzf"
 const defaultSetting = {
   continuousDialogue: true,
   archiveSession: false,
-  openaiAPIKey: "",
+  openaiAPIKey: "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",
   openaiAPITemperature: 60,
   systemRule: ""
 }
