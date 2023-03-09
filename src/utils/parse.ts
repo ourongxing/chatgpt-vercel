1a7f388ad94779bf5418d1531fe3de40700869a1
ourongxing <orongxing@gmail.com>
2023-03-09 19:45:35 +0800

    pref: parse prompts from markdown

---
+++ src/utils/parse.ts
@@ -0,0 +1,24 @@
+import * as md from "../../prompts.md"
+
+export async function parsePrompts() {
+  return md
+    .rawContent()
+    .split("## Prompts")[1]
+    .split(/^### (.+)$/m)
+    .filter(k => k.trim())
+    .reduce(
+      (acc, cur, i) => {
+        if (i % 2 === 0)
+          acc.push({
+            desc: cur.trim(),
+            prompt: ""
+          })
+        else acc[acc.length - 1].prompt = cur.trim()
+        return acc
+      },
+      [] as {
+        desc: string
+        prompt: string
+      }[]
+    )
+}
