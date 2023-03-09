08fc4d90890b5296e67faffad7905a57ce91bc51
ourongxing <orongxing@gmail.com>
2023-03-09 21:08:21 +0800

    chore: split chunk

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,22 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              const arr = id.toString().split("node_modules/")[1].split("/")
+              if (arr[1].includes("markdown")) return "__markdown"
+              if (arr[1].includes("highlight")) return "__highlight"
+              return "__vendor"
+            }
+          }
+        }
+      }
+    }
+  },
   integrations: [
     unocss({
       presets: [
