78ec4a434d6b667e97ce33ac31940c0877260d21
ourongxing <orongxing@gmail.com>
2023-03-09 11:17:22 +0800

    chore: update defaultMessage

--- src/default.ts
+++ src/default.ts
@@ -11,5 +11,4 @@ export const defaultMessage = `
 - 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 - 由 [ourongxing](https://github.com/ourongxing) 修改自 [Diu](https://github.com/ddiu8081/chatgpt-demo)，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。
-- 如果本项目对你有所帮助，可以给小猫买点零食，但不接受任何付费功能请求。
-![](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)`
+- 如果本项目对你有所帮助，可以给小猫[买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。`
