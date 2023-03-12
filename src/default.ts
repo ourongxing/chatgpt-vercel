6d19b803487fc3248ae631e706c48a8a99c3dd3d
MarkNFT <103547118+MarkNFT@users.noreply.github.com>
2023-03-12 15:12:03 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -9,5 +9,5 @@ export const defaultSetting = {
 export const defaultMessage = `
 - 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 - 由 [@ourongxing](https://github.com/ourongxing) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
-- 如果本项目对你有所帮助，公益性服务随时可能停止运行
+- 由 [马克云](https://mp.weixin.qq.com/s/t8Hmk7JOvel7_pi5I5Vhpg)提供api，免费服务随时可能停止运行
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
