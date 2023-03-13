74f10caa6f2df6bafb00d2ae053aa22b41cdf6f1
ourongxing <orongxing@gmail.com>
2023-03-13 09:04:12 +0800

    revert: style

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -47,7 +47,7 @@ export default ({ role, message }: Props) => {
 
   return (
     <div
-      class="flex py-2 gap-3 px-0 md:px-4 rounded-lg transition-colors md:hover:bg-slate/5 dark:md:hover:bg-slate/2 relative message-item"
+      class="flex py-2 gap-3 px-4 rounded-lg transition-colors md:hover:bg-slate/5 dark:md:hover:bg-slate/2 relative message-item"
       // class:op-75={role === "user"}
     >
       <div
