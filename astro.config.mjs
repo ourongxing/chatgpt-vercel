aded843e66798fefc68462796e04294047dc0472
ourongxing <orongxing@gmail.com>
2023-03-20 11:18:00 +0800

    chore: discard edge function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config"
-import vercel from "@astrojs/vercel/edge"
+import vercel from "@astrojs/vercel/serverless"
 import node from "@astrojs/node"
-import netlify from "@astrojs/netlify/edge-functions"
+import netlify from "@astrojs/netlify/functions"
 import cloudflare from "@astrojs/cloudflare"
 import unocss from "unocss/astro"
 import {
