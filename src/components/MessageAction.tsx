67040b4557e3c8db997f886ff7d9d038560d2a29
ourongxing <orongxing@gmail.com>
2023-03-25 23:06:39 +0800

    pref: style

--- src/components/MessageAction.tsx
+++ src/components/MessageAction.tsx
@@ -11,7 +11,7 @@ export default function MessageAction(props: {
   const [copied, setCopied] = createSignal(false)
   return (
     <Show when={!props.hidden}>
-      <div class="flex absolute items-center justify-between top--4 right-0 text-sm text-slate-7 dark:text-slate group-hover:opacity-100 group-focus:opacity-100 opacity-0 bg-slate/10 rounded">
+      <div class="flex absolute items-center justify-between <sm:top--4 <sm:right-0 top-2 right-2 text-sm text-slate-7 dark:text-slate group-hover:opacity-100 group-focus:opacity-100 opacity-0 dark:bg-#292B32 bg-#E7EBF0 rounded">
         <Show when={props.role === "assistant"}>
           <ActionItem
             label="复制"
@@ -48,7 +48,7 @@ export default function MessageAction(props: {
 function ActionItem(props: { onClick: any; icon: string; label?: string }) {
   return (
     <div
-      class="flex items-center cursor-pointer p-2 hover:bg-slate hover:bg-op-20 rounded text-1.2em"
+      class="flex items-center cursor-pointer p-2 hover:bg-slate/10 rounded text-1.2em"
       onClick={props.onClick}
     >
       <button class={props.icon} title={props.label} />
