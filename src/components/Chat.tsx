1b770cb013ad96b2f8736c9769e0bdfedcb128a0
ourongxing <orongxing@gmail.com>
2023-03-25 19:38:59 +0800

    pref: style

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -395,117 +395,119 @@ export default function (props: {
         </div>
       </div>
       <div
-        class="pb-2em px-2em fixed bottom-0 z-100 op-0"
-        style={
-          containerWidth() === "init"
-            ? {}
-            : {
-                transition: "opacity 1s ease-in-out",
-                width: containerWidth(),
-                opacity: 100,
-                "background-color": "var(--c-bg)"
-              }
-        }
+        class="pb-2em px-2em fixed bottom-0 z-100"
+        style={{
+          "background-color": "var(--c-bg)",
+          width: containerWidth() === "init" ? "100%" : containerWidth()
+        }}
       >
-        <Show when={!compatiblePrompt().length && height() === "48px"}>
-          <SettingAction
-            setting={setting}
-            setSetting={setSetting}
-            clear={clearSession}
-            reAnswer={reAnswer}
-            messaages={messageList()}
-          />
-        </Show>
-        <Show
-          when={!loading()}
-          fallback={() => (
-            <div class="h-12 flex items-center justify-center bg-slate bg-op-15 text-slate rounded">
-              <span>AI 正在思考...</span>
+        <div
+          style={{
+            transition: "opacity 1s ease-in-out",
+            opacity: containerWidth() === "init" ? 0 : 100
+          }}
+        >
+          <Show when={!compatiblePrompt().length && height() === "48px"}>
+            <SettingAction
+              setting={setting}
+              setSetting={setSetting}
+              clear={clearSession}
+              reAnswer={reAnswer}
+              messaages={messageList()}
+            />
+          </Show>
+          <Show
+            when={!loading()}
+            fallback={() => (
+              <div class="h-12 flex items-center justify-center bg-slate bg-op-15 text-slate rounded">
+                <span>AI 正在思考...</span>
+                <div
+                  class="ml-1em px-2 py-0.5 border border-slate text-slate rounded-md text-sm op-70 cursor-pointer hover:bg-slate/10"
+                  onClick={stopStreamFetch}
+                >
+                  不需要了
+                </div>
+              </div>
+            )}
+          >
+            <Show when={compatiblePrompt().length}>
+              <PromptList
+                prompts={compatiblePrompt()}
+                select={selectPrompt}
+              ></PromptList>
+            </Show>
+            <div class="flex items-end">
+              <textarea
+                ref={inputRef!}
+                id="input"
+                placeholder="与 ta 对话吧"
+                autocomplete="off"
+                value={inputContent()}
+                autofocus
+                onClick={scrollToBottom}
+                onKeyDown={e => {
+                  if (e.isComposing) return
+                  if (compatiblePrompt().length) {
+                    if (
+                      e.key === "ArrowUp" ||
+                      e.key === "ArrowDown" ||
+                      e.key === "Enter"
+                    ) {
+                      e.preventDefault()
+                    }
+                  } else if (e.key === "Enter") {
+                    if (!e.shiftKey) {
+                      e.preventDefault()
+                      handleButtonClick()
+                    }
+                  } else if (e.key === "ArrowUp") {
+                    const userMessages = messageList()
+                      .filter(k => k.role === "user")
+                      .map(k => k.content)
+                    const content = userMessages.at(-1)
+                    if (content && !inputContent()) {
+                      e.preventDefault()
+                      setInputContent(content)
+                    }
+                  }
+                }}
+                onInput={handleInput}
+                style={{
+                  height: height(),
+                  "border-bottom-right-radius": 0,
+                  "border-top-right-radius":
+                    height() === "48px" ? 0 : "0.25rem",
+                  "border-top-left-radius":
+                    compatiblePrompt().length === 0 ? "0.25rem" : 0
+                }}
+                class="self-end py-3 resize-none w-full px-3 text-slate-7 dark:text-slate bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none placeholder:text-slate-400 placeholder:text-slate-400 placeholder:op-40"
+                rounded-l
+              />
+              <Show when={inputContent()}>
+                <button
+                  class="i-carbon:add-filled absolute right-5em bottom-3em rotate-45 text-op-20! hover:text-op-80! text-slate-7 dark:text-slate"
+                  onClick={() => {
+                    setInputContent("")
+                    inputRef.focus()
+                  }}
+                />
+              </Show>
               <div
-                class="ml-1em px-2 py-0.5 border border-slate text-slate rounded-md text-sm op-70 cursor-pointer hover:bg-slate/10"
-                onClick={stopStreamFetch}
+                class="flex text-slate-7 dark:text-slate bg-slate bg-op-15 text-op-80! hover:text-op-100! h-3em items-center rounded-r"
+                style={{
+                  "border-top-right-radius":
+                    compatiblePrompt().length === 0 ? "0.25rem" : 0
+                }}
               >
-                不需要了
+                <button
+                  title="发送"
+                  onClick={() => handleButtonClick()}
+                  class="i-carbon:send-filled text-5 mx-3"
+                />
               </div>
             </div>
-          )}
-        >
-          <Show when={compatiblePrompt().length}>
-            <PromptList
-              prompts={compatiblePrompt()}
-              select={selectPrompt}
-            ></PromptList>
           </Show>
-          <div class="flex items-end">
-            <textarea
-              ref={inputRef!}
-              id="input"
-              placeholder="与 ta 对话吧"
-              autocomplete="off"
-              value={inputContent()}
-              autofocus
-              onClick={scrollToBottom}
-              onKeyDown={e => {
-                if (e.isComposing) return
-                if (compatiblePrompt().length) {
-                  if (
-                    e.key === "ArrowUp" ||
-                    e.key === "ArrowDown" ||
-                    e.key === "Enter"
-                  ) {
-                    e.preventDefault()
-                  }
-                } else if (e.key === "Enter") {
-                  if (!e.shiftKey) {
-                    e.preventDefault()
-                    handleButtonClick()
-                  }
-                } else if (e.key === "ArrowUp") {
-                  const userMessages = messageList()
-                    .filter(k => k.role === "user")
-                    .map(k => k.content)
-                  const content = userMessages.at(-1)
-                  if (content && !inputContent()) {
-                    e.preventDefault()
-                    setInputContent(content)
-                  }
-                }
-              }}
-              onInput={handleInput}
-              style={{
-                height: height(),
-                "border-bottom-right-radius": 0,
-                "border-top-right-radius": height() === "48px" ? 0 : "0.25rem",
-                "border-top-left-radius":
-                  compatiblePrompt().length === 0 ? "0.25rem" : 0
-              }}
-              class="self-end py-3 resize-none w-full px-3 text-slate-7 dark:text-slate bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none placeholder:text-slate-400 placeholder:text-slate-400 placeholder:op-40"
-              rounded-l
-            />
-            <Show when={inputContent()}>
-              <button
-                class="i-carbon:add-filled absolute right-5em bottom-3em rotate-45 text-op-20! hover:text-op-80! text-slate-7 dark:text-slate"
-                onClick={() => {
-                  setInputContent("")
-                  inputRef.focus()
-                }}
-              />
-            </Show>
-            <div
-              class="flex text-slate-7 dark:text-slate bg-slate bg-op-15 text-op-80! hover:text-op-100! h-3em items-center rounded-r"
-              style={{
-                "border-top-right-radius":
-                  compatiblePrompt().length === 0 ? "0.25rem" : 0
-              }}
-            >
-              <button
-                title="发送"
-                onClick={() => handleButtonClick()}
-                class="i-carbon:send-filled text-5 mx-3"
-              />
-            </div>
-          </div>
-        </Show>
+        </div>
       </div>
     </div>
   )
--- src/components/SettingAction.tsx
+++ src/components/SettingAction.tsx
@@ -16,122 +16,126 @@ export default function SettingAction(props: {
   const [copied, setCopied] = createSignal(false)
   const [imgCopied, setIMGCopied] = createSignal(false)
   return (
-    <div class="text-sm text-slate-7 dark:text-slate mb-2">
+    <div class="text-sm text-slate-7 dark:text-slate my-2">
       <Show when={shown()}>
-        <SettingItem icon="i-ri:lock-password-line" label="网站密码">
-          <input
-            type="password"
-            value={props.setting().password}
-            class="max-w-150px ml-1em px-1 text-slate-7 dark:text-slate rounded-sm bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none"
-            onInput={e => {
-              props.setSetting({
-                ...props.setting(),
-                password: (e.target as HTMLInputElement).value
-              })
-            }}
-          />
-        </SettingItem>
-        <SettingItem icon="i-carbon:api" label="OpenAI Key">
-          <input
-            type="password"
-            value={props.setting().openaiAPIKey}
-            class="max-w-150px ml-1em px-1 text-slate-7 dark:text-slate rounded-sm bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none"
-            onInput={e => {
-              props.setSetting({
-                ...props.setting(),
-                openaiAPIKey: (e.target as HTMLInputElement).value
-              })
-            }}
-          />
-        </SettingItem>
-        <SettingItem icon="i-carbon:machine-learning-model" label="OpenAI 模型">
-          <select
-            name="model"
-            class="max-w-150px w-full bg-slate bg-op-15 rounded-sm appearance-none accent-slate text-center  focus:bg-op-20 focus:ring-0 focus:outline-none"
-            value={props.setting().model}
-            onChange={e => {
-              props.setSetting({
-                ...props.setting(),
-                model: (e.target as HTMLSelectElement).value as Model
-              })
-            }}
+        <div class="<sm:max-h-10em max-h-14em overflow-y-auto">
+          <SettingItem icon="i-ri:lock-password-line" label="网站密码">
+            <input
+              type="password"
+              value={props.setting().password}
+              class="max-w-150px ml-1em px-1 text-slate-7 dark:text-slate rounded-sm bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none"
+              onInput={e => {
+                props.setSetting({
+                  ...props.setting(),
+                  password: (e.target as HTMLInputElement).value
+                })
+              }}
+            />
+          </SettingItem>
+          <SettingItem icon="i-carbon:api" label="OpenAI Key">
+            <input
+              type="password"
+              value={props.setting().openaiAPIKey}
+              class="max-w-150px ml-1em px-1 text-slate-7 dark:text-slate rounded-sm bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none"
+              onInput={e => {
+                props.setSetting({
+                  ...props.setting(),
+                  openaiAPIKey: (e.target as HTMLInputElement).value
+                })
+              }}
+            />
+          </SettingItem>
+          <SettingItem
+            icon="i-carbon:machine-learning-model"
+            label="OpenAI 模型"
           >
-            <option value="gpt-3.5-turbo">gpt-3.5-turbo(4k)</option>
-            <option value="gpt-4">gpt-4(8k)</option>
-            <option value="gpt-4-32k">gpt-4(32k)</option>
-          </select>
-        </SettingItem>
-        <SettingItem icon="i-carbon:user-online" label="系统角色指令">
-          <input
-            type="text"
-            value={props.setting().systemRule}
-            class="text-ellipsis max-w-150px ml-1em px-1 text-slate-7 dark:text-slate rounded-sm bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none"
-            onInput={e => {
-              props.setSetting({
-                ...props.setting(),
-                systemRule: (e.target as HTMLInputElement).value
-              })
-            }}
-          />
-        </SettingItem>
-        <SettingItem icon="i-carbon:data-enrichment" label="思维发散程度">
-          <input
-            type="range"
-            min={0}
-            max={100}
-            value={String(props.setting().openaiAPITemperature)}
-            class="max-w-150px w-full h-2 bg-slate bg-op-15 rounded-lg appearance-none cursor-pointer accent-slate"
-            onInput={e => {
-              props.setSetting({
-                ...props.setting(),
-                openaiAPITemperature: Number(
-                  (e.target as HTMLInputElement).value
-                )
-              })
-            }}
-          />
-        </SettingItem>
-        <SettingItem
-          icon="i-carbon:save-image"
-          label="记录对话内容，刷新不会消失"
-        >
-          <label class="relative inline-flex items-center cursor-pointer ml-1">
-            <input
-              type="checkbox"
-              checked={props.setting().archiveSession}
-              class="sr-only peer"
+            <select
+              name="model"
+              class="max-w-150px w-full bg-slate bg-op-15 rounded-sm appearance-none accent-slate text-center  focus:bg-op-20 focus:ring-0 focus:outline-none"
+              value={props.setting().model}
               onChange={e => {
                 props.setSetting({
                   ...props.setting(),
-                  archiveSession: (e.target as HTMLInputElement).checked
+                  model: (e.target as HTMLSelectElement).value as Model
                 })
               }}
-            />
-            <div class="w-9 h-5 bg-slate bg-op-15 peer-focus:outline-none peer-focus:ring-0  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-slate"></div>
-          </label>
-        </SettingItem>
-        <SettingItem
-          icon="i-carbon:3d-curve-auto-colon"
-          label="开启连续对话，将加倍消耗 Token"
-        >
-          <label class="relative inline-flex items-center cursor-pointer ml-1">
+            >
+              <option value="gpt-3.5-turbo">gpt-3.5-turbo(4k)</option>
+              <option value="gpt-4">gpt-4(8k)</option>
+              <option value="gpt-4-32k">gpt-4(32k)</option>
+            </select>
+          </SettingItem>
+          <SettingItem icon="i-carbon:user-online" label="系统角色指令">
             <input
-              type="checkbox"
-              checked={props.setting().continuousDialogue}
-              class="sr-only peer"
-              onChange={e => {
+              type="text"
+              value={props.setting().systemRule}
+              class="text-ellipsis max-w-150px ml-1em px-1 text-slate-7 dark:text-slate rounded-sm bg-slate bg-op-15 focus:bg-op-20 focus:ring-0 focus:outline-none"
+              onInput={e => {
                 props.setSetting({
                   ...props.setting(),
-                  continuousDialogue: (e.target as HTMLInputElement).checked
+                  systemRule: (e.target as HTMLInputElement).value
                 })
               }}
             />
-            <div class="w-9 h-5 bg-slate bg-op-15 peer-focus:outline-none peer-focus:ring-0  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-slate"></div>
-          </label>
-        </SettingItem>
-        <hr class="mt-2 bg-slate-5 bg-op-15 border-none h-1px"></hr>
+          </SettingItem>
+          <SettingItem icon="i-carbon:data-enrichment" label="思维发散程度">
+            <input
+              type="range"
+              min={0}
+              max={100}
+              value={String(props.setting().openaiAPITemperature)}
+              class="max-w-150px w-full h-2 bg-slate bg-op-15 rounded-lg appearance-none cursor-pointer accent-slate"
+              onInput={e => {
+                props.setSetting({
+                  ...props.setting(),
+                  openaiAPITemperature: Number(
+                    (e.target as HTMLInputElement).value
+                  )
+                })
+              }}
+            />
+          </SettingItem>
+          <SettingItem
+            icon="i-carbon:save-image"
+            label="记录对话内容，刷新不会消失"
+          >
+            <label class="relative inline-flex items-center cursor-pointer ml-1">
+              <input
+                type="checkbox"
+                checked={props.setting().archiveSession}
+                class="sr-only peer"
+                onChange={e => {
+                  props.setSetting({
+                    ...props.setting(),
+                    archiveSession: (e.target as HTMLInputElement).checked
+                  })
+                }}
+              />
+              <div class="w-9 h-5 bg-slate bg-op-15 peer-focus:outline-none peer-focus:ring-0  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-slate"></div>
+            </label>
+          </SettingItem>
+          <SettingItem
+            icon="i-carbon:3d-curve-auto-colon"
+            label="开启连续对话，将加倍消耗 Token"
+          >
+            <label class="relative inline-flex items-center cursor-pointer ml-1">
+              <input
+                type="checkbox"
+                checked={props.setting().continuousDialogue}
+                class="sr-only peer"
+                onChange={e => {
+                  props.setSetting({
+                    ...props.setting(),
+                    continuousDialogue: (e.target as HTMLInputElement).checked
+                  })
+                }}
+              />
+              <div class="w-9 h-5 bg-slate bg-op-15 peer-focus:outline-none peer-focus:ring-0  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-slate"></div>
+            </label>
+          </SettingItem>
+        </div>
       </Show>
-      <div class="mt-2 flex items-center justify-between">
+      <div class="flex items-center justify-between">
         <ActionItem
           onClick={() => {
             setShown(!shown())
