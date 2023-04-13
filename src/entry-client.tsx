a7120c8f24a1e51f0a4a111190f07675d2be4c57
Ou <orongxing@gmail.com>
2023-04-13 18:29:06 +0800

    pref: add Array.prototype.at() polyfill

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -6,4 +6,13 @@ if (!e || e === "auto" ? a : e === "dark") {
   document.documentElement.classList.add("dark")
 }
 
+if (!Array.prototype.at) {
+  Array.prototype.at = function (index) {
+    index = index < 0 ? index + this.length : index
+    if (index >= 0 && index < this.length) {
+      return this[index]
+    }
+  }
+}
+
 mount(() => <StartClient />, document)
